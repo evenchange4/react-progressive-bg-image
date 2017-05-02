f0db516b41de2b1593fd25f69c1e388ef4f090e3
Michael Hsu (徐承志) <evenchange4@gmail.com>
2017-05-02 17:12:06 +0800

    docs(CHANGELOG): release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,8 +3,12 @@
 ## [HEAD]
 > Unreleased
 
+## [v1.0.1]
+> May 02, 2017
+
 * Setup travis CI and netlify.
 * Update readme.md
+* Setup jest for testing.
 
 ## [v1.0.0]
 > May 02, 2017
--- src/__tests__/ProgressiveImage.test.js
+++ src/__tests__/ProgressiveImage.test.js
@@ -4,10 +4,7 @@ import ProgressiveImage from '../ProgressiveImage';
 
 it('should render <ProgressiveImage>', () => {
   const wrapper = mount(
-    <ProgressiveImage
-      src="origin.jpg"
-      placeholder="small.jpg"
-    />,
+    <ProgressiveImage src="origin.jpg" placeholder="small.jpg" />,
   );
 
   expect(wrapper).toMatchSnapshot();
