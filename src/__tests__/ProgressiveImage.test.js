9d228d9a8d35279573de678de1068c62ec0fd35f
Michael Hsu <evenchange4@gmail.com>
2017-05-02 17:09:17 +0800

    test(jest): setup (#2)

---
+++ src/__tests__/ProgressiveImage.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import ProgressiveImage from '../ProgressiveImage';
+
+it('should render <ProgressiveImage>', () => {
+  const wrapper = mount(
+    <ProgressiveImage
+      src="origin.jpg"
+      placeholder="small.jpg"
+    />,
+  );
+
+  expect(wrapper).toMatchSnapshot();
+});
